2d7fb31ebef3fc8fa94724b0411eecb1a6e7ed22
Josh Komolafe <josh.komolafe@gmail.com>
2026-09-02 02:34:13 +0100

    Make Life Concierge the active Discover tab

--- expo/app/(tabs)/_layout.tsx
+++ expo/app/(tabs)/_layout.tsx
@@ -24,7 +24,7 @@ const PRIMARY_TABS = [
     icon: LayoutGrid,
   },
   {
-    name: "discover-flagship",
+    name: "discover-concierge",
     title: "Discover",
     icon: Compass,
   },
@@ -36,6 +36,7 @@ const PRIMARY_TABS = [
 ] as const;
 
 const LEGACY_TABS = [
+  "discover-concierge",
   "discover-flagship",
   "discover-life",
   "discover-home",
